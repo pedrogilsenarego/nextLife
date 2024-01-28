4382f2708dbd55692c7a92f567eceb267c809d76
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-01-28 22:49:32 +0000

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/SignUpUserSteps";
-import Header from "@/components/Header";
+import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
+import AuthButton from "../components/AuthButton";
 
 export default async function Index() {
   const cookieStore = cookies();
@@ -26,13 +24,11 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
           {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
@@ -48,7 +44,7 @@ export default async function Index() {
             className="font-bold hover:underline"
             rel="noreferrer"
           >
-            Supabase
+            SenaRego
           </a>
         </p>
       </footer>
