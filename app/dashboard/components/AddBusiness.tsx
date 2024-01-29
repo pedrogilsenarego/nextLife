ed7fe3ee32496167c82fc39c94048e4de772a51b
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-01-29 17:55:41 +0000

    a

--- app/dashboard/components/AddBusiness.tsx
+++ app/dashboard/components/AddBusiness.tsx
@@ -1,4 +1,4 @@
-import { ControlledInput } from "@/components/UI/Inputs/ControlledInput";
+import { Input } from "@/components/UI/Input";
 import Popup from "@/components/UI/Popup";
 
 import { Button } from "@/components/UI/button";
@@ -6,7 +6,7 @@ import { Button } from "@/components/UI/button";
 export default async function () {
   return (
     <Popup trigger={<Button>+</Button>} title="Add new Business">
-      <ControlledInput placeholder="Name" />
+      <Input placeholder="Name" />
     </Popup>
   );
 }
--- components/UI/Input.tsx
+++ components/UI/Input.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const UncontrolledInput = React.forwardRef<HTMLInputElement, InputProps>(
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
@@ -20,6 +20,6 @@ const UncontrolledInput = React.forwardRef<HTMLInputElement, InputProps>(
     );
   }
 );
-UncontrolledInput.displayName = "Input";
+Input.displayName = "Input";
 
-export { UncontrolledInput };
+export { Input };
