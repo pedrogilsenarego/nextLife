31bc51324a63e505ef3afb86f31eff092445c419
pedro <87972406+pedrogilsenarego@users.noreply.github.com>
2024-01-29 16:02:10 +0000

    Cn (#4)
    * a
    
    * a

---
+++ app/dashboard/components/AddBusiness.tsx
@@ -0,0 +1,35 @@
+import IconButton from "@/components/ui/IconButton";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+export default async function () {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <IconButton />
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
--- app/dashboard/components/MainCard.tsx
+++ app/dashboard/components/MainCard.tsx
@@ -1,18 +1,20 @@
-import Card from "@/components/UI/Card";
-import IconButton from "@/components/UI/IconButton";
+import Card from "@/components/ui/Card";
+import AddBusiness from "./AddBusiness";
 
 export default async function () {
   return (
-    <Card>
-      <div className="flex border-b-2 w-full justify-between pb-1">
-        <div className="flex items-center gap-2">
-          <p className="cursor-pointer">General</p>
-          <IconButton />
+    <>
+      <Card>
+        <div className="flex border-b-2 w-full justify-between pb-1">
+          <div className="flex items-center gap-2">
+            <p className="cursor-pointer">General</p>
+            <AddBusiness />
+          </div>
+          <div className="flex items-center">
+            <p className="cursor-pointer">Settings</p>
+          </div>
         </div>
-        <div className="flex items-center">
-          <p className="cursor-pointer">Settings</p>
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </>
   );
 }
