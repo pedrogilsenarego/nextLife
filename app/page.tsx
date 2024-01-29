69f249b4145cbebfd22cbdf4c072e13bab2f5041
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-01-29 00:34:41 +0000

    a

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default async function Index() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-6xl flex justify-between items-center p-3 text-sm">
+        <div className="w-full max-w-6xl flex justify-end items-center p-3 text-sm">
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
--- components/ChartComponents/RoundShow.tsx
+++ components/ChartComponents/RoundShow.tsx
@@ -5,18 +5,20 @@ type Props = {
 };
 
 export default async function Ball({ value, currency, percentage = 0 }: Props) {
+  const greenColor = "green";
   const gradientStyle = {
-    background: `conic-gradient(from 0deg, red 0%, red ${percentage}%, green ${percentage}%, green ${
+    background: `conic-gradient(from 0deg, red 0%, red ${percentage}%, ${greenColor} ${percentage}%, ${greenColor} ${
       100 - percentage
     }%)`,
   };
+  const textStyle = { color: percentage > 50 ? "red" : "green" };
   return (
     <div
       className="flex items-center justify-center w-48 h-48 rounded-full"
       style={gradientStyle}
     >
       <div className="flex items-center justify-center w-40 h-40 bg-white rounded-full">
-        <p className="text-lg font-semibold">
+        <p className="text-3xl font-semibold" style={textStyle}>
           {currency}
           {value}
         </p>
