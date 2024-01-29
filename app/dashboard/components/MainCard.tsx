aa9e53f267d9af0dd4fb00bc67c5bac6268354a3
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-01-29 23:58:44 +0000

    a

--- app/dashboard/components/MainCard.tsx
+++ app/dashboard/components/MainCard.tsx
@@ -1,7 +1,6 @@
 import Card from "@/components/UI/Card";
-import { Button } from "@/components/UI/button";
 import { getBusinesses } from "@/server/businessActions";
-import AddBusiness from "./AddBusiness";
+import CardHeader from "./CardHeader";
 
 export default async function () {
   const businesses = await getBusinesses();
@@ -9,26 +8,7 @@ export default async function () {
   return (
     <>
       <Card>
-        <div className="flex gap-2 w-full">
-          <AddBusiness />
-          <div className="flex border-b-2 w-full justify-between ">
-            <div className="flex items-center gap-4">
-              <Button variant={"ghost"}>General</Button>
-              {businesses.map((business) => (
-                <Button
-                  variant={"ghost"}
-                  key={business.id}
-                  className="cursor-pointer"
-                >
-                  {business.businessName}
-                </Button>
-              ))}
-            </div>
-            <div className="flex items-center">
-              <p className="cursor-pointer">Settings</p>
-            </div>
-          </div>
-        </div>
+        <CardHeader businesses={businesses} />
       </Card>
     </>
   );
