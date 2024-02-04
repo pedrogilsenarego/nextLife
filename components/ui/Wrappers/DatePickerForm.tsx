8a6bb44da20efd61a8b48c1a066060c55a3ae9b1
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-02-04 16:34:38 +0000

    a

--- components/ui/Wrappers/DatePickerForm.tsx
+++ components/ui/Wrappers/DatePickerForm.tsx
@@ -53,9 +53,7 @@ const DatePickerForm = ({ control, name, label, description }: Props) => {
                 mode="single"
                 selected={field.value}
                 onSelect={field.onChange}
-                disabled={(date) =>
-                  date > new Date() || date < new Date("1900-01-01")
-                }
+                disabled={(date) => date < new Date("1900-01-01")}
                 initialFocus
               />
             </PopoverContent>
