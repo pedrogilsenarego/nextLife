5005d73420e7986c13a4dd4e467a47b98761795e
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-02-15 18:54:01 +0000

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
-import Footer from "@/components/LayoutComponents/Footer";
-import Header from "@/components/LayoutComponents/Header";
 import { Toaster } from "@/components/ui/toaster";
 import ReactQueryProvider from "@/providers/ReactQueryProvider";
 import { GeistSans } from "geist/font/sans";
-import { headers } from "next/headers";
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
@@ -15,23 +12,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const heads = headers();
-
-  const pathname = heads.get("next-url");
-  const isHomePage = pathname === "/";
-
   return (
     <>
       <html lang="en" className={GeistSans.className}>
         <body className="bg-background text-foreground">
           <main className="min-h-screen flex flex-col items-center">
-            <ReactQueryProvider>
-              <div className="flex-1 w-full  flex flex-col gap-20 items-center">
-                {!isHomePage && <Header />}
-                {children}
-                {!isHomePage && <Footer />}
-              </div>
-            </ReactQueryProvider>
+            <ReactQueryProvider>{children}</ReactQueryProvider>
           </main>
           <Toaster />
         </body>
