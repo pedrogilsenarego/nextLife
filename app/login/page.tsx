cdae562df2e106f9e905287d978ee28dca091d4d
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-02-16 23:46:10 +0000

    a

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,16 @@
 import { Card } from "@/components/ui/card";
 import LoginPage from "@/modules/LoginPage";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (session) redirect(`/${session.user.user_metadata.displayName}`);
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <Card className="flex flex-col p-4 justify-center">
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import HomePage from "@/modules/HomePage";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Index() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (session) redirect(`/${session.user.user_metadata.displayName}`);
   return (
     <div className=" w-full h-screen flex flex-col  items-center">
       <HomePage />
