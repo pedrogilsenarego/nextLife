82b1a02e6dd1a9a363789a64d994b5512f437c62
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-02-18 22:12:30 +0000

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,17 @@ import { Toaster } from "@/components/ui/toaster";
 import ReactQueryProvider from "@/providers/ReactQueryProvider";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { Metadata } from "next";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
+
+export const metadata: Metadata = {
+  title: "Next Life",
+  description: "Money flow control app",
+  manifest: "/manifest.json",
+};
 
 export default function RootLayout({
   children,
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+const prod = process.env.NODE_ENV === 'production'
+
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  //disable: prod ? false : true,
+  skipWaiting: true
+});
+
+module.exports = withPWA({
+  //next.js config
+});
