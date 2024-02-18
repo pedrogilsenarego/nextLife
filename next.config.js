9370887d1f5455508948dbda4b8c764d6ce8a3ac
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-02-18 22:22:15 +0000

    a

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const prod = process.env.NODE_ENV === 'production'
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
-  //disable: prod ? false : true,
+  disable: prod ? false : true,
   skipWaiting: true
 });
 
