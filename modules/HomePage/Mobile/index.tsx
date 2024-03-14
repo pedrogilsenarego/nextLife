0e695dca876c4567ed8c3e291b7468efd58a14ad
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-03-14 15:53:18 +0000

    a

--- modules/HomePage/Mobile/index.tsx
+++ modules/HomePage/Mobile/index.tsx
@@ -1,9 +1,13 @@
 import LoginForm from "@/modules/LoginPage/components/LoginForm/LoginForm";
 import NewUserLink from "@/modules/LoginPage/components/NewUserLink";
+import Logo from "../../../assets/logo.svg";
+import Image from "next/image";
 
 const HomePageMobile = () => {
   return (
-    <div className="w-full gap-6 p-8 h-full flex flex-col justify-center items-center">
+    <div className="w-full gap-6 p-8 h-full flex flex-col justify-around items-center">
+      <Image src={Logo} alt="logo" className="w-full" />
+
       <LoginForm />
       <NewUserLink />
     </div>
