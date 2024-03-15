4e5644983c80b77010bbf9f3afbdc24b91d7841d
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-03-15 16:18:23 +0000

    a

--- modules/LoginPage/components/NewUserLink/index.tsx
+++ modules/LoginPage/components/NewUserLink/index.tsx
@@ -7,13 +7,13 @@ import { useRouter } from "next/navigation";
 const NewUserLink = () => {
   const router = useRouter();
   return (
-    <P
+    <p
       className="mt-2 text-sm text-center"
       style={{ cursor: "pointer" }}
       onClick={() => router.push(ROUTE_PATHS.SIGNUP)}
     >
       If you are a new user <b>click here</b>
-    </P>
+    </p>
   );
 };
 
