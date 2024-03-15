6625ccf581accf6d6798534d6783797d3c13b550
pedrogilsenarego <pedrogilsenarego@gmail.com>
2024-03-15 16:17:40 +0000

    a

--- modules/HomePage/Mobile/index.tsx
+++ modules/HomePage/Mobile/index.tsx
@@ -6,20 +6,20 @@ import { Card } from "@/components/ui/card";
 
 const HomePageMobile = () => {
   return (
-    <div className="w-full gap-6 p-4 h-full flex flex-col justify-between items-center">
-      <div className="flex flex-col justify-center h-full gap-4">
+    <div className="w-full gap-6 p-4 h-full flex flex-col justify-between items-center my-6">
+      <div className="flex flex-col justify-start h-full gap-6">
         <Image
-          style={{ marginBottom: "30px" }}
+          style={{ marginBottom: "10px", marginTop: "50px" }}
           src={Logo}
           alt="logo"
           className="w-full px-4"
         />
         <Card className="p-4 rounded-none shadow-md flex flex-col justify-between gap-4">
           <LoginForm />
-          <NewUserLink />
         </Card>
+        <NewUserLink />
       </div>
-      <p className="text-xs">
+      <p className="text-xs p-6">
         Powered by <a className="font-bold hover:underline">SenaRego</a>
       </p>
     </div>
--- modules/LoginPage/components/NewUserLink/index.tsx
+++ modules/LoginPage/components/NewUserLink/index.tsx
@@ -8,11 +8,11 @@ const NewUserLink = () => {
   const router = useRouter();
   return (
     <P
-      className="mt-2 text-sm"
+      className="mt-2 text-sm text-center"
       style={{ cursor: "pointer" }}
       onClick={() => router.push(ROUTE_PATHS.SIGNUP)}
     >
-      If you are a new user click here
+      If you are a new user <b>click here</b>
     </P>
   );
 };
